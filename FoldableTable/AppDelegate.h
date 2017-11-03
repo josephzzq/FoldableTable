975ca7ded14ab69e2878d5fc2412866b5c43e7bc
josephzzq <joseph.tsai@tgc-taiwan.com.tw>
2017-11-03 15:34:16 +0800

    Initial Commit

---
+++ FoldableTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoldableTable
+//
+//  Created by Joseph.Tsai on 2017/11/3.
+//  Copyright © 2017年 Joseph.Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
